d178d6ecdf94140609ef6774b6e9891894c57b96
ByungJun Cho <aischool815@gmail.com>
2018-07-29 20:50:18 +0900

    setting up register page

---
+++ client/src/components/Base.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <H1>Register</H1>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
+
